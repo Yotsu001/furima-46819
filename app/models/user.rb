ecf0ff6444b39adb312b869f0f742ff72409f712
Yotsu001 <re.af.sna.g7n.9@gmail.com>
2026-01-23 10:05:20 +0900

    モデル単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :password_confirmation, presence: true
+  validates :birth_date, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
                                          message: '全角文字を使用してください' } do
