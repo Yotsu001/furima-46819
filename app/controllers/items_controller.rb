bdf28f89ee952f92ee7b6b3c017dc395d616d91c
Yotsu001 <re.af.sna.g7n.9@gmail.com>
2026-01-25 16:18:53 +0900

    商品情報の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,8 +13,14 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
   end
   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,13 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :delivery_time
   belongs_to :user
   has_one_attached :image
 
+  validates :image,           presence: true
   validates :name,            presence: true
   validates :description,     presence: true
   validates :category_id,     numericality: { other_than: 1, message: "can't be blank" }
