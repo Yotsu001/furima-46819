d810b481aa35d2d26ca69d20ad63509f6e321447
Yotsu001 <re.af.sna.g7n.9@gmail.com>
2026-01-25 23:02:01 +0900

    innerHTMLの作成

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,14 @@
 window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById('item-price');
+  if (!priceInput) return;
 
-  priceInput.addEventListener("input", () => {
-   const inputValue = priceInput.value;
-})
+  const addTaxDom = document.getElementById('add-tax-price');
+  const profitDom = document.getElementById('profit');
 
-  const taxpriceDisplay = document.getElementById('add-tax-price');
-  const profitDisplay = document.getElementById('profit');
+  priceInput.addEventListener('input', () => {
+    const price  = Number(priceInput.value);
+    
+    addTaxDom.innerHTML = Math.floor(price * 0.1);
+    profitDom.innerHTML = Math.floor(price * 0.9);
+  });
 });
