2ae6e501016cc76861226d79718e64be09111ae6
Yotsu001 <re.af.sna.g7n.9@gmail.com>
2026-01-25 22:40:16 +0900

    イベント発火

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,10 @@
 window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById('item-price');
+
+  priceInput.addEventListener("input", () => {
+   console.log("イベント発火");
+})
+
   const taxpriceDisplay = document.getElementById('add-tax-price');
   const profitDisplay = document.getElementById('profit');
 });
