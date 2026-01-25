6d8fc27bc00450e6d01d4a46043db2904963b791
Yotsu001 <re.af.sna.g7n.9@gmail.com>
2026-01-25 22:36:27 +0900

    JSファイルを作成し、必要な要素を取得

---
+++ app/javascript/item_price.js
@@ -0,0 +1,5 @@
+window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById('item-price');
+  const taxpriceDisplay = document.getElementById('add-tax-price');
+  const profitDisplay = document.getElementById('profit');
+});
