69793bba822c5946963ebbf9c06c3f5c6ce5ba6f
Yotsu001 <re.af.sna.g7n.9@gmail.com>
2026-02-09 09:39:26 +0900

    モデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,8 @@ class Item < ApplicationRecord
   belongs_to :delivery_time
   belongs_to :user
   has_one_attached :image
+  
+  has_one :purchase
 
   validates :image,           presence: true
   validates :name,            presence: true
