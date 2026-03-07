16a76a7e6b94b812ec2581670fcf28c3b9c01145
Yotsu001 <re.af.sna.g7n.9@gmail.com>
2026-03-07 18:36:29 +0900

    aws.rbへの追記

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,11 @@
 require 'aws-sdk-s3'
+require 'openssl'
 
+# 1. AWS SDKに対して直接指定
 Aws.config.update({
   ssl_ca_bundle: '/etc/pki/tls/cert.pem'
 })
+
+# 2. RubyのOpenSSLライブラリ全体に、OSが成功したパスを強制注入
+# これにより、Rails内のあらゆるSSL通信（S3以外も含む）がこのパスを使います
+OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ca_file] = '/etc/pki/tls/cert.pem'
